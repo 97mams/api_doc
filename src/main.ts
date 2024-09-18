d8e03b3ae1ee4b731d0fcf335df2a560339f98a9
97mams <97mams@gmail.com>
2024-09-18 19:22:34 +0300

    change variable

--- src/main.ts
+++ src/main.ts
@@ -12,39 +12,113 @@ const toCopy = (btn: HTMLButtonElement, text: HTMLParagraphElement) => {
     }
 }
 
-const getRender = (curentId: number): String | undefined => {
+const getRender = (currentId: number): String | undefined => {
     if (main) {
         return main.innerHTML = `
     <h2 class="text-2xl mb-2">Get teams</h2>
-    <p class="text-gray-500" id="text-${curentId}">
+    <p class="text-gray-500" id="text-${currentId}">
         Returns a list of all stored football teams.
     </p>
     <div class="py-4 px-4 my-4 bg-gray-400 rounded-md flex justify-between" >
         <p>http://localhost:3000/teams</p>
-        <button class="bg-gray-100 rounded-sm px-2" id="copy-${curentId}">copy</button>
+        <button class="bg-gray-100 rounded-sm px-2" id="copy-${currentId}">copy</button>
     </div>
         `;
     }
-    const btnToCopy = document.getElementById(`copy-${curentId}`) as HTMLButtonElement
-    const textToCopy = document.getElementById(`text-${curentId}`) as HTMLParagraphElement
+    const btnToCopy = document.getElementById(`copy-${currentId}`) as HTMLButtonElement
+    const textToCopy = document.getElementById(`text-${currentId}`) as HTMLParagraphElement
+    toCopy(btnToCopy, textToCopy);
+}
+
+const postRender = (currentId: number): String | undefined => {
+    if (main) {
+        return main.innerHTML = `
+    <h2 class="text-2xl mb-2">Add a new team</h2>
+    <p class="text-gray-500">Adds a new team to the database.</p>
+    <div class="py-4 px-4 my-4 bg-gray-400 rounded-md flex justify-between" >
+        <p>http://localhost:3000/team</p>
+        <button class="bg-gray-100 rounded-sm px-2" id="copy-${currentId}">copy</button>
+    </div>
+    <p>Method: POST</p>
+    <p>Body: json</br>
+    {
+        "name": "Team Name"
+    }
+    </p>
+    <p>Response: json</br>
+    {
+    "message": "Team successfully added",
+    "team": {
+      "id": 2,
+      "name": "Team Name",
+      "wins": 0,
+      "losses": 0,
+      "draws": 0
+      "point": 0
+    }
+  }
+    </p>
+        `;
+    }
+    const btnToCopy = document.getElementById(`copy-${currentId}`) as HTMLButtonElement
+    const textToCopy = document.getElementById(`text-${currentId}`) as HTMLParagraphElement
+    toCopy(btnToCopy, textToCopy);
+}
+
+const putRender = (currentId: number): String | undefined => {
+    if (main) {
+        return main.innerHTML = `
+    <h2 class="text-2xl mb-2">Update a team's statistics</h2>
+    <p class="text-gray-500" id="text-${currentId}">
+        Updates an existing team's information, including the number of wins, losses, draws and points.
+    </p>
+    <div class="py-4 px-4 my-4 bg-gray-400 rounded-md flex justify-between" >
+        <p>http://localhost:3000/team?id={id}</p>
+        <button class="bg-gray-100 rounded-sm px-2" id="copy-${currentId}">copy</button>
+    </div>
+    <p>Method: POST</p>
+    <p>Body: json</br>
+    {
+        "win": 1,
+        "lose": 0,
+        "draw": 0
+    }
+    </p>
+    <p>Response: json</br>
+    {
+	"message": "Team success uptaded",
+	"team": {
+		"win": 1,
+		"lose": 1,
+		"draws": 2,
+		"point": 5,
+		"match": 4,
+		"id": "29"
+	    }
+    }
+    </p>
+        `;
+    }
+    const btnToCopy = document.getElementById(`copy-${currentId}`) as HTMLButtonElement
+    const textToCopy = document.getElementById(`text-${currentId}`) as HTMLParagraphElement
     toCopy(btnToCopy, textToCopy);
 }
 
 for (let elementActive in btn) {
     btn[elementActive].addEventListener('click', () => {
         const endpoint = btn[elementActive].textContent
-        const curentId = parseInt(elementActive)
+        const currentId = parseInt(elementActive)
 
         switch (endpoint) {
             case "Get":
-                getRender(curentId)
+                getRender(currentId)
                 break;
             case "Post":
-                console.log(endpoint);
+                postRender(currentId);
 
                 break;
             case "Put":
-                console.log(endpoint);
+                putRender(currentId);
                 break;
             default:
                 break;
